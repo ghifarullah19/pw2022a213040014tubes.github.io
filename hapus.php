817888d40167f7ce641934f286b82a63accea9b5
ghifarullah19 <ghifarullah11@gmail.com>
2022-06-01 23:31:37 +0700

    commit pertama

---
+++ hapus.php
@@ -0,0 +1,29 @@
+<?php 
+session_start();
+
+require 'functions.php';
+
+if (!(isset($_SESSION["login"]))) {
+  header("Location: login.php");
+  exit;
+}
+
+$id_movie = $_GET["id"];
+
+if (hapus($id_movie) > 0) {
+  echo "
+      <script>
+        alert('Data Berhasil Dihapus!');
+        document.location.href = 'film.php';
+      </script>
+    ";
+} else {
+  echo "
+      <script>
+        alert('Data Gagal Dihapus!');
+        document.location.href = 'index.php';
+      </script>
+    ";
+}
+
+?>
